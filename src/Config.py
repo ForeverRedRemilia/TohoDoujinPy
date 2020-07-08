3eee91f572d7a89985b0015d0525b43a70052c95
RedRemilia <redremilia@toho.xyz>
2020-07-08 17:06:10 +0800

    renamed search.py

--- src/Config.py
+++ src/Config.py
@@ -1,4 +1,7 @@
-# 浏览器请求头信息
+# 浏览器请求头信息，Cookie需要登录表站后才能获取，表站地址为e-hentai.org，如果访问不了请挂v2ray等工具
+# 在表站中登录后，使用浏览器的扩展插件（Firefox、Chrome扩展商店皆有）更改Cookie，将ipb_mennber_id和ipb_pass_hash站点更改为exhentai.org
+# 使用https://exhentai.org访问，选好要查询的条件，点击查询，copy地址栏中的url，根据需求更改url
+# 以下示例是用 Firefox 79 访问得到的请求头信息
 headers = {
     "Host": "exhentai.org",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
