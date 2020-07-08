1d98cafa787fee471573b7cc382b944ee7839291
RedRemilia <redremilia@toho.xyz>
2020-07-08 18:50:21 +0800

    #

--- src/Config.py
+++ src/Config.py
@@ -1,7 +1,7 @@
 # 浏览器请求头信息，Cookie需要登录表站后才能获取，表站地址为e-hentai.org，如果访问不了请挂v2ray等工具
 # 在表站中登录后，使用浏览器的扩展插件（Firefox、Chrome扩展商店皆有）更改Cookie，将ipb_mennber_id和ipb_pass_hash站点更改为exhentai.org
 # 使用https://exhentai.org访问，选好要查询的条件，点击查询，copy地址栏中的url，根据需求更改url
-# 以下示例是用 Firefox 79 访问得到的请求头信息
+# 以下示例是用 Firefox 79 访问得到的请求头信息，可直接从浏览器控制台——网络中截取
 headers = {
     "Host": "exhentai.org",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
@@ -9,14 +9,15 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
-    #"Referer": "https://exhentai.org/?f_cats=761&f_search=parody%3Atouhou_project+character%3AMarisa_Kirisame&advsearch=1&f_sname=on&f_stags=on&f_sh=on&f_spf=&f_spt=",
+    # Referer与url一致即可
     "Referer": "",
-    "Cookie": "ipb_member_id=1838963; ipb_pass_hash=4620a8f43eedbf611ef866f0ced267d4; igneous=af81e450f; sk=547jkxp2r2bpsm401qza9k0rkajc; u=1838963-0-539k0n29hm9",
+    # Cookie是核心参数一定要有
+    "Cookie": "",
     "Upgrade-Insecure-Requests": "1"
 }
 
 # 代理设置
-proxy = '127.0.0.1:10809'  # 本地代理
+proxy = '127.0.0.1:10809'  # 本地代理 可以更换成自己的
 proxies = {
     'http': 'http://' + proxy,
     'https': 'https://' + proxy
